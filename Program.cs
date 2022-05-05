e1aa974733c18c5e18f145aca60d66a0e785b3f7
Evgeny <joy08@mail.ru>
2022-05-05 12:34:52 +1100

    Обявлен второй массивэ

--- Program.cs
+++ Program.cs
@@ -5,14 +5,21 @@ Console.WriteLine("Введите длину массива: ");
 int startLengthArr = int.Parse(Console.ReadLine()); // длина начального масива массива
 int endLengthArr = 0; // длина массива с результатом
 
-string[] startArray = new string[startLengthArr];
+string[] startArray = new string[startLengthArr]; //Начальный массив
 
-for(int i = 0; i < startArray.Length; i++)
+for(int i = 0; i < startArray.Length; i++) // Заполняем массив
 {
     Console.WriteLine("Введите элемент массива: ");
     startArray[i] = Console.ReadLine();
 }
 
+for(int i = 0; i < startArray.Length; i++) // Ищем количсвто элемент в которых количесвто символов =< 3
+{
+    char[] chars = startArray[i].ToCharArray();
+    for(int j = 0; j <chars.Length; j++)
+    {
+        if(j >= 3) endLengthArr++;
+    }
+}
 
-
-string[] endArray = new string[endLengthArr];
+string[] endArray = new string[endLengthArr]; // Массив с результатом
