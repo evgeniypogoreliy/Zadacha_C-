4b7ae861335ce7db93e9b3e0edb67b598a17fa7b
Evgeny <joy08@mail.ru>
2022-05-05 12:10:03 +1100

    Заполнили начальный массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует масив строк,
+// длина котлорых меньше либо равна 3 символа
+
+Console.WriteLine("Введите длину массива: ");
+int startLengthArr = int.Parse(Console.ReadLine()); // длина начального масива массива
+int endLengthArr = 0; // длина массива с результатом
+
+string[] startArray = new string[startLengthArr];
+
+for(int i = 0; i < startArray.Length; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+    startArray[i] = Console.ReadLine();
+}
+
+
+
+string[] endArray = new string[endLengthArr];
